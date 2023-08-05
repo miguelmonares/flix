bed141af7bfdf2e54d47b2f59608a4a62853f01e
mxnares <69873782+mxnares@users.noreply.github.com>
2023-08-05 13:34:51 -0700

    feat add: stimulus page styling

--- src/components/Stimulus/Stimulus.js
+++ src/components/Stimulus/Stimulus.js
@@ -10,15 +10,11 @@ function Stimulus() {
   };
 
   return (
-    <div className="Home">
-      <header className="Home-header">
-        <h1>Stimulus</h1>
-        <p>
-          Experience a rollercoaster of emotions and capture your reactions in
-          real-time!
-        </p>
+    <div class="body">
+      <div class="instruction-video-div">
+        <div></div>
         <button onClick={startExperience}>Results</button>
-      </header>
+      </div>
     </div>
   );
 }
