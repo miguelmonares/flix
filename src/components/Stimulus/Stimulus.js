c4461b0f51d9b43c49541f5e0df73614521d9c4d
mxnares <69873782+mxnares@users.noreply.github.com>
2023-08-05 23:14:30 -0700

    feat add: stimulus page
    added start experience action, video player, and auto-end nav.
    
    todo: camera recording and server upload.

--- src/components/Stimulus/Stimulus.js
+++ src/components/Stimulus/Stimulus.js
@@ -1,20 +1,72 @@
-import React from "react";
+import React, { useRef, useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import "./Stimulus.css";
 
 function Stimulus() {
+  const videoRef = useRef(null);
+  const videoPath = "src/flix-smiling-woman.mp4";
   const navigate = useNavigate();
+  const [videoStarted, setVideoStarted] = useState(false);
+  const [videoEnded, setVideoEnded] = useState(false);
 
-  const startExperience = () => {
-    navigate("/results", { replace: true });
+  const handleStartClick = () => {
+    setVideoStarted(true);
+    const videoElement = videoRef.current;
+
+    // Play video
+    videoElement.play().catch((err) => {
+      console.error("Failed to play video:", err);
+    });
+
+    // Request fullscreen
+    if (videoElement.requestFullscreen) {
+      videoElement.requestFullscreen();
+    } else if (videoElement.mozRequestFullScreen) {
+      videoElement.mozRequestFullScreen();
+    } else if (videoElement.webkitRequestFullscreen) {
+      videoElement.webkitRequestFullscreen();
+    } else if (videoElement.msRequestFullscreen) {
+      videoElement.msRequestFullscreen();
+    }
+
+    // Add event listener to set videoEnded to true when video ends
+    videoElement.addEventListener("ended", () => {
+      setVideoEnded(true);
+    });
   };
 
+  useEffect(() => {
+    // Automatically navigate to the results page once the video ends
+    if (videoEnded) {
+      navigate("/results", { replace: true });
+    }
+  }, [videoEnded, navigate]);
+
   return (
-    <div class="body">
-      <div class="instruction-video-div">
-        <div></div>
-        <button onClick={startExperience}>Results</button>
-      </div>
+    <div className="stimulus-container">
+      {!videoStarted && (
+        <div className="start-experience-overlay">
+          <button
+            onClick={handleStartClick}
+            className="start-experience-button"
+          >
+            start the experience
+          </button>
+        </div>
+      )}
+      <video ref={videoRef} controls={true} className="video">
+        <source src={videoPath} type="video/mp4" />
+        your browser does not support the video tag.
+      </video>
+      <button
+        onClick={() => navigate("/results", { replace: true })}
+        disabled={!videoEnded}
+        className={`about-button results-button ${
+          videoEnded ? "" : "disabled"
+        }`}
+      >
+        {videoEnded ? "go to results" : "finish the video"}
+      </button>
     </div>
   );
 }
